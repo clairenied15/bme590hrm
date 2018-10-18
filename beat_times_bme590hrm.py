c5cbe1d2ea0dc01d20019b2c0d7dc6e4e07d1464
Claire Niederriter <you@example.com>
2018-10-18 18:15:14 -0400

    added for loop to find the times at each heart beat

--- beat_times_bme590hrm.py
+++ beat_times_bme590hrm.py
@@ -1,14 +1,33 @@
 import numpy as np
-from num_beats_bme590hrm import loc
-from num_beats_bme590hrm import dif
+from numpy import diff
+from readCSV_bme590hrm import time
 
-
-def beat_times():
-    #for x in np.nditer(dif):
-     #   if x < 3:
-    #dat = np.load('num_beats_bme590hrm.npy')
-    #loc = dat['array1']
-    #dif = dat['array2']
+def beat_times(loc,dif):
     tloc = np.where(dif < 3)
-    print(tloc)
+    tlocn = np.delete(tloc, 0, 0)
+    locary = np.array(loc)
+    loclist = locary.tolist()
+    tmary = np.array(time)
+    #btme = [time[x] for x in loclist]
+    lints = [int(i) for i in loclist]
+    for x in lints:
+        btime = time[x]
+    #for x in locary:
+     #   btime = time[x]
+    #rep = diff(tlocn)
+    #oneary = np.array([1])
+    #strt = np.insert(stpplus,0,oneary)
+    #stp = np.where(dif > 3)
+    #stp = np.delete(stp, 0, 0)
+    #strt = stp + 1
+    #strt = np.insert(strt,0,0)
+    #nd = np.ma.count(loc)
+    #stp = np.append(stp, nd-1)
+    #loclist = list(loc)
+    #for x in strt:
+     #   for y in stp:
+      #      splitloc = loc[x:y]
+    
+    print(time)
             # find where x<3 and average each group 
+			# maybe make arays/lists for each beat
