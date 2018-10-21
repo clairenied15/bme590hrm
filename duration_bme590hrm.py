139b6dba53da61a2519aa730d680d35c9854779f
Claire Niederriter <you@example.com>
2018-10-21 18:07:30 -0400

    add Doctring to duration py file

--- duration_bme590hrm.py
+++ duration_bme590hrm.py
@@ -1,4 +1,13 @@
 def duration_hrm(time):
+    """Find the duration of an ECG strip by subtracting the initial time from the final time
+	
+	Args:
+	    time: array of time values read from a CSV file
+	
+	Returns:
+	    duration: the duration of the imported ECG signal (in seconds)
+	
+	"""
     duration = time[-1] - time[0]
     #print(time[-1])
     #print(time[0])
