d4ccfc1f97aa5887e4280cffd2cafa6ef7f5fd4f
Claire Niederriter <you@example.com>
2018-10-21 17:53:53 -0400

    added Docstring to readCSV file

--- readCSV_bme590hrm.py
+++ readCSV_bme590hrm.py
@@ -3,6 +3,16 @@ import pandas as pd
 from pandas import read_csv
 
 def load_CSV():
+    """ Load in a csv file with time and voltage ECG data
+	
+	Args:
+	    None
+	
+	Returns:
+	    time: an array of the time data
+		voltage: an array of the voltage data 
+		
+	"""
     filename = 'test_data1.csv'
     df = pd.read_csv(filename, delimiter = ',', header = None)
     tme = df.iloc[:,0]
