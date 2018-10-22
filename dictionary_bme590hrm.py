5c27f7391fb5258da146060ca63d425ac026c3b2
Claire Niederriter <you@example.com>
2018-10-22 17:16:49 -0400

    added code to save json file and changed dictionary keys

--- dictionary_bme590hrm.py
+++ dictionary_bme590hrm.py
@@ -1,11 +1,15 @@
 import json
 
 def hrm_dictionary(beats, num_beats, duration, voltage_extremes, mean_hr_bpm, filename):
-    metrics = {beats: "numpy array of times when a beat occurred",
-                num_beats: "number of detected beats in the ECG strip",
-                duration: "time duration of the ECG strip",
-                voltage_extremes: "tuple containing minimum and maximum lead voltages",
-                mean_hr_bpm: "estimated average heart rate"}
+    metrics = {
+        "beats (numpy array of times when a beat occurred)": beats,
+        "num_beats (number of detected beats in the ECG strip)": num_beats,
+        "duration (time duration of the ECG strip)": duration,
+        "voltage_extremes (tuple containing minimum and maximum lead voltages)": voltage_extremes,
+        "mean_hr_bpm (estimated average heart rate)": mean_hr_bpm
+	}
     #return metrics
-    with open(filename.json, 'w') as fp:
-    json.dump(metrics, fp)
+    jname = filename + ".json"
+    print(jname)
+    with open(jname, 'w') as fp:
+        json.dump(metrics, fp)
