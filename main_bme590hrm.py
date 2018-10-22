e1f9d5ccd4cb767e5dc2a412dc1af0cf5b02af90
Claire Niederriter <you@example.com>
2018-10-21 20:47:50 -0400

    add Docstring to main py file

--- main_bme590hrm.py
+++ main_bme590hrm.py
@@ -8,6 +8,9 @@ from readCSV_bme590hrm import load_CSV
 from volt_extremes_bme590hrm import volt_extremes
 
 def main():
+    """Run all of the functions to find the mean heart rate, voltage extremes, duration, number of beats, and beat times for an imported ECG signal
+	
+	"""
     time, voltage = load_CSV()
     fs = 1000.0
     lowcut = 0.5
