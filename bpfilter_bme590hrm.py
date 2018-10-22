2a074c422f84834c94f3d9fac6d76e0ba31c2002
Claire Niederriter <you@example.com>
2018-10-21 21:03:15 -0400

    add Docstring to bpfilter file

--- bpfilter_bme590hrm.py
+++ bpfilter_bme590hrm.py
@@ -1,6 +1,18 @@
 from scipy.signal import butter, lfilter
 
 def butter_bandpass(lowcut, highcut, fs, order=5):
+    """Create a butterworth bandpass filter
+	
+	Args:
+	    lowcut: low frequency cutoff
+		highcut: high frequency cutoff
+		fs: sampling frequency
+		order: filter order (power)
+	
+	Returns:
+	    b, a: butterworth bandpass filter coefficients
+	
+	"""
     nyq = 0.5 * fs
     low = lowcut / nyq
     high = highcut / nyq
@@ -9,6 +21,19 @@ def butter_bandpass(lowcut, highcut, fs, order=5):
 
 
 def butter_bandpass_filter(voltage, lowcut, highcut, fs, order=5):
+    """Filter data with a bandpass, butterworth filter
+	
+	Args:
+	    voltage: array of voltage data from an ECG signal
+		lowcut: low frequency cutoff
+		highcut: high frequency cutoff
+		fs: sampling frequency
+		order: filter order (power)
+	
+	Returns:
+	    filtdat: array of filtered voltage data
+	
+	"""
     b, a = butter_bandpass(lowcut, highcut, fs, order=order)
     filtdat = lfilter(b, a, voltage)
     return filtdat
