5a8e091d615022a543e54033142486060bab27a9
Claire Niederriter <you@example.com>
2018-10-22 18:06:30 -0400

    added Docstring to disctionary file

--- dictionary_bme590hrm.py
+++ dictionary_bme590hrm.py
@@ -1,6 +1,20 @@
 import json
 
 def hrm_dictionary(beats, num_beats, duration, voltage_extremes, mean_hr_bpm, filename):
+    """Create a dictionary with the different values found for the ECG signal
+	
+	Args:
+	    beats: list of times when a heart beat occurred
+		num_beats: total number of detected heart beats in the ECG strip
+		duration: time duration of the ECG strip 
+		voltage_extremes: tuple with the maximum and minimum lead voltage values
+		mean_hr_bpm: mean heart rate in beats per minute
+		filename: name of the CSV file the ECG data is being read from
+	
+	Returns:
+	    jname: json file with the dictionary for the ECG values in it
+	
+	"""
     metrics = {
         "beats (numpy array of times when a beat occurred)": beats,
         "num_beats (number of detected beats in the ECG strip)": num_beats,
@@ -13,3 +27,4 @@ def hrm_dictionary(beats, num_beats, duration, voltage_extremes, mean_hr_bpm, fi
     print(jname)
     with open(jname, 'w') as fp:
         json.dump(metrics, fp)
+	return jname
