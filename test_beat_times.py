ff8ff7f2d62befa9360ea9a9ee69ab64a1fa9ec6
Claire Niederriter <you@example.com>
2018-10-22 15:31:17 -0400

    add test_beat_times file

---
+++ test_beat_times.py
@@ -0,0 +1,6 @@
+from beat_times_bme590hrm import beat_times 
+import numpy
+
+def test_beat_times():
+    s = beat_times(list(range(1,101)),[[3,4,5,18,19,20,30,32,34,55,57,59,73,76,77,92,93,94]],numpy.array([1,1,13,1,1,10,2,2,21,2,2,14,3,1,15,1,1]))
+    assert s == [6,21,35,60,78,95]
