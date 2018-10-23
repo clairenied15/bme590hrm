ce08686ed649aba682f69abd3660add278695b55
Claire Niederriter <you@example.com>
2018-10-22 22:55:30 -0400

    add test_readCSV file

---
+++ test_readCSV.py
@@ -0,0 +1,9 @@
+from readCSV_bme590hrm import load_CSV
+import pandas as pd
+from pandas import read_csv
+
+def test_read_CSV():
+    s = load_CSV()
+    df = s[3]
+    numcols = len(df.columns)
+    assert numcols == 2
