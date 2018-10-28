41d6c36741af3b36319633928b5bb3062306d7fb
Claire Niederriter <42756895+clairenied15@users.noreply.github.com>
2018-10-28 14:27:06 -0400

    Update readCSV_bme590hrm.py

--- readCSV_bme590hrm.py
+++ readCSV_bme590hrm.py
@@ -2,23 +2,24 @@ import csv
 import pandas as pd
 from pandas import read_csv
 
+
 def load_CSV():
     """ Load in a csv file with time and voltage ECG data
-	
-	Args:
-	    None
-	
-	Returns:
-	    time: a list of the time data
-		voltage: a list of the voltage data 
-		
-	"""
+
+        Args:
+            None
+
+        Returns:
+            time: a list of the time data
+            voltage: a list of the voltage data 
+
+    """
     filename = 'test_data1.csv'
     df = pd.read_csv(filename, delimiter = ',', header = None)
     l = len(df.columns)
-    tme = df.iloc[:,0]
+    tme = df.iloc[:, 0]
     time = [float(i) for i in tme]
-    volt = df.iloc[:,1]
+    volt = df.iloc[:, 1]
     voltage = [float(i) for i in volt]
-    #print(type(voltage))
+    # print(type(voltage))
     return time, voltage, filename, df
