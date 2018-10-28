6728b23c767faac5e0b3f5cd86be26562d03c98b
Claire Niederriter <42756895+clairenied15@users.noreply.github.com>
2018-10-28 13:54:14 -0400

    Update duration_bme590hrm.py

--- duration_bme590hrm.py
+++ duration_bme590hrm.py
@@ -1,5 +1,6 @@
 def duration_hrm(time):
-    """Find the duration of an ECG strip by subtracting the initial time from the final time
+    """Find the duration of an ECG strip by subtracting
+        the initial time from the final time
 
         Args:
             time: list of time values read from a CSV file
