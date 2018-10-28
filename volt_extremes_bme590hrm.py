d6bd703e32682e69b2f2baf86f1912286f690f9f
Claire Niederriter <42756895+clairenied15@users.noreply.github.com>
2018-10-28 15:25:19 -0400

    Update volt_extremes_bme590hrm.py

--- volt_extremes_bme590hrm.py
+++ volt_extremes_bme590hrm.py
@@ -1,17 +1,20 @@
 def volt_extremes(filtdat):
-    """Find the maximum and minimum voltage values for the filtered ECG signal
-	
-	Args:
-	    filtdat: array of filtered voltage values from imported ECG signal
-	
-	Returns:
-	    voltage_extremes: tuple containing minimum and maximum lead voltages from ECG signal
-	
-	"""
+    """Find the maximum and minimum voltage values for
+    the filtered ECG signal
+
+        Args:
+            filtdat: array of filtered voltage values from
+	    imported ECG signal
+
+        Returns:
+            voltage_extremes: tuple containing minimum and
+	    maximum lead voltages from ECG signal
+
+    """
     max_volt = max(filtdat)
     min_volt = min(filtdat)
     voltage_extremes = (min_volt, max_volt)
-    #print(max_volt)
-    #print(min_volt)
+    # print(max_volt)
+    # print(min_volt)
     print(voltage_extremes)
     return voltage_extremes
