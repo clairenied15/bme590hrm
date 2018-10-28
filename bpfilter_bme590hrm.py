04c3433191f97885876b0825033235c0bdf8b8ae
Claire Niederriter <42756895+clairenied15@users.noreply.github.com>
2018-10-28 13:17:06 -0400

    Update bpfilter_bme590hrm.py

--- bpfilter_bme590hrm.py
+++ bpfilter_bme590hrm.py
@@ -23,18 +23,18 @@ def butter_bandpass(lowcut, highcut, fs, order=5):
 
 def butter_bandpass_filter(voltage, lowcut, highcut, fs, order=5):
     """Filter data with a bandpass, butterworth filter
-	
-	Args:
-	    voltage: array of voltage data from an ECG signal
-		lowcut: low frequency cutoff
-		highcut: high frequency cutoff
-		fs: sampling frequency
-		order: filter order (power)
-	
-	Returns:
-	    filtdat: array of filtered voltage data
-	
-	"""
+
+        Args:
+            voltage: array of voltage data from an ECG signal
+            lowcut: low frequency cutoff
+            highcut: high frequency cutoff
+            fs: sampling frequency
+            order: filter order (power)
+
+        Returns:
+            filtdat: array of filtered voltage data
+
+    """
     b, a = butter_bandpass(lowcut, highcut, fs, order=order)
     filtdat = lfilter(b, a, voltage)
     return filtdat
