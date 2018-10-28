33a31db0b4d3638f165af715e965ed54e9b7babb
Claire Niederriter <42756895+clairenied15@users.noreply.github.com>
2018-10-28 13:29:27 -0400

    Update dictionary_bme590hrm.py

--- dictionary_bme590hrm.py
+++ dictionary_bme590hrm.py
@@ -1,8 +1,8 @@
 import json
 
 
-def hrm_dictionary(beats, num_beats, duration, voltage_extremes, 
-                    mean_hr_bpm, filename):
+def hrm_dictionary(beats, num_beats, duration, voltage_extremes,
+mean_hr_bpm, filename):
     """Create a dictionary with the different values found for the ECG signal
 
         Args:
@@ -22,8 +22,8 @@ def hrm_dictionary(beats, num_beats, duration, voltage_extremes,
         "beats (numpy array of times when a beat occurred)": beats,
         "num_beats (number of detected beats in the ECG strip)": num_beats,
         "duration (time duration of the ECG strip)": duration,
-        "voltage_extremes (tuple containing minimum and maximum lead voltages)":
-            voltage_extremes,
+        "voltage_extremes (tuple containing minimum and maximum lead voltages)"
+        : voltage_extremes,
         "mean_hr_bpm (estimated average heart rate)": mean_hr_bpm
         }
     jname = filename + ".json"
